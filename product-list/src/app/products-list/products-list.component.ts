1c81201915d8c5c8c49a666ee5e9359c0af31c58
Moonseo Kim <vv_eros@naver.com>
2020-03-24 15:25:30 +0900

    Complete products-list component

--- product-list/src/app/products-list/products-list.component.ts
+++ product-list/src/app/products-list/products-list.component.ts
@@ -16,5 +16,21 @@ export class ProductsListComponent {
   @Input() productList: Product[];
   // tslint:disable-next-line:no-output-on-prefix
   @Output() onProductSelected: EventEmitter<Product>;
-  constructor() { }
+  private currentProduct: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
+  }
+
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean{
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
 }
