bac33f74637c36b9591b1e97dea890eba85a4bff
Moonseo Kim <vv_eros@naver.com>
2020-03-24 15:10:53 +0900

    Generate products-list

---
+++ product-list/src/app/products-list/products-list.component.ts
@@ -0,0 +1,20 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.css']
+})
+export class ProductsListComponent {
+  @Input() productList: Product[];
+  // tslint:disable-next-line:no-output-on-prefix
+  @Output() onProductSelected: EventEmitter<Product>;
+  constructor() { }
+}
