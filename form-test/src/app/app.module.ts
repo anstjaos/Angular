7c33c7342f3bc53c4d416cc3ca5aaeef3b035ffc
Moonseo Kim <vv_eros@naver.com>
2020-03-26 18:57:52 +0900

    ngForm test

--- form-test/src/app/app.module.ts
+++ form-test/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormSkuComponent } from './form-sku/form-sku.component';
+import { SuiModule } from 'ng2-semantic-ui';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormSkuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
