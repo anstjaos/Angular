6339f3dd73da8420486c76e5c99d417500f56cea
Moonseo Kim <vv_eros@naver.com>
2020-03-26 19:04:41 +0900

    Make form-sku-with-builder component

--- form-test/src/app/app.module.ts
+++ form-test/src/app/app.module.ts
@@ -8,11 +8,13 @@ import {
 import { AppComponent } from './app.component';
 import { FormSkuComponent } from './form-sku/form-sku.component';
 import { SuiModule } from 'ng2-semantic-ui';
+import { FormSkuWithBuilderComponent } from './form-sku-with-builder/form-sku-with-builder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormSkuComponent
+    FormSkuComponent,
+    FormSkuWithBuilderComponent
   ],
   imports: [
     BrowserModule,
