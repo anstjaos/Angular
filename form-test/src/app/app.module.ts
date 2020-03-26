8bb15600fa0835a60624e37739f0072085795eff
Moonseo Kim <vv_eros@naver.com>
2020-03-26 19:24:56 +0900

    Add form-ng-model component

--- form-test/src/app/app.module.ts
+++ form-test/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppComponent } from './app.component';
 import { FormSkuComponent } from './form-sku/form-sku.component';
 import { SuiModule } from 'ng2-semantic-ui';
 import { FormSkuWithBuilderComponent } from './form-sku-with-builder/form-sku-with-builder.component';
+import { FormNgModelComponent } from './form-ng-model/form-ng-model.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormSkuComponent,
-    FormSkuWithBuilderComponent
+    FormSkuWithBuilderComponent,
+    FormNgModelComponent
   ],
   imports: [
     BrowserModule,
---
+++ form-test/src/app/form-ng-model/form-ng-model.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-form-ng-model',
+  templateUrl: './form-ng-model.component.html',
+  styleUrls: ['./form-ng-model.component.css']
+})
+export class FormNgModelComponent implements OnInit {
+  myForm: FormGroup;
+  productName: string;
+
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'productName':['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(value: string): void {
+    console.log('you submitted value: ', value);
+  }
+}
