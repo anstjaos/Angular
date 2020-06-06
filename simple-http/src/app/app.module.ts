9fe463c8cc25d97f4c489d43876e5796102dad5a
moonseo.kim <moonseo.kim@nhn.com>
2020-06-06 17:06:13 +0900

    Init simple-http project

---
+++ simple-http/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SimpleHttpComponent } from './simple-http/simple-http.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SimpleHttpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ simple-http/src/app/simple-http/simple-http.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-simple-http',
+  templateUrl: './simple-http.component.html',
+  styleUrls: ['./simple-http.component.css']
+})
+export class SimpleHttpComponent implements OnInit {
+  data: object;
+  loading: boolean;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+}
