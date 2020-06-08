94274d3b12e2988414cad8bb5ad9ed0ee39b0972
moonseo.kim <moonseo.kim@nhn.com>
2020-06-08 14:09:47 +0900

    Add search box component

---
+++ simple-http/src/app/search-box/search-box.component.ts
@@ -0,0 +1,42 @@
+import {Component, ElementRef, EventEmitter, OnInit, Output} from '@angular/core';
+import {SearchResult} from '../you-tube-search/search-result.model';
+import {YouTubeSearchService} from '../you-tube-search/you-tube-search.service';
+import {fromEvent} from 'rxjs';
+import {map, filter, debounceTime, tap, switchAll} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrls: ['./search-box.component.css']
+})
+export class SearchBoxComponent implements OnInit {
+  @Output() loading: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() results: EventEmitter<SearchResult[]> = new EventEmitter<SearchResult[]>();
+
+  constructor(private youtube: YouTubeSearchService,
+              private el: ElementRef) { }
+
+  ngOnInit(): void {
+    const obs = fromEvent(this.el.nativeElement, 'keyup')
+      .pipe(
+        map((e: any) => e.target.value),
+        filter((text: string) => text.length > 1),
+        debounceTime(250),
+        tap(() => this.loading.next(true)),
+        map((query: string) => this.youtube.search(query)),
+        switchAll()
+          ). subscribe(
+            (results: SearchResult[]) => {
+              this.loading.next(false);
+              this.results.next(results);
+            },
+            (err: any) => {
+              console.log(err);
+              this.loading.next(false);
+            },
+            () => {
+              this.loading.next(false);
+            }
+          );
+  }
+}
